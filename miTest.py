78b3a3909de18a1a22100c3a80f81b9638eaf2df
dimon-ton <101252386+dimon-ton@users.noreply.github.com>
2023-07-20 14:07:25 +0700

    open pages to fill in

--- miTest.py
+++ miTest.py
@@ -1,17 +1,31 @@
 
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-import chromedriver_autoinstaller
+from selenium.webdriver.edge.service import Service
 
-chromedriver_autoinstaller.install()
+path = r'C:\Users\saich\Documents\miTest\msedgedriver.exe'
 
+service = Service(path)
 
-driver = webdriver.Chrome()
+driver = webdriver.Edge(service=service)
 
 url = r'https://mi-test.obec.go.th/student/login.php'
 
 driver.get(url)
+
+user_elem = driver.find_element(By.NAME, 'user_em')
+
+password_elem = driver.find_element(By.NAME, 'password_em')
+
+user_elem.send_keys("test typing")
+
+
+
+
+
+
 
 time.sleep(5)
 
